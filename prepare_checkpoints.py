593cdfa55ef71b263208d9fe0d5602e2d341afdc
hungphongtrn <phonglearntodev@gmail.com>
2024-12-02 23:11:27 +0700

    refactor: add additional yaml file to control experiments

--- prepare_checkpoints.py
+++ prepare_checkpoints.py
@@ -1,11 +1,11 @@
 from huggingface_hub import snapshot_download
 
 snapshot_download(
-    repo_id="google/siglip-base-patch16-256-multilingual",
+    repo_id="google/siglip-so400m-patch14-384",
     local_dir="siglip_checkpoints",
     max_workers=2,
 )
 
-snapshot_download(
-    repo_id="openai/clip-vit-base-patch16", local_dir="clip_checkpoints", max_workers=2
-)
+# snapshot_download(
+#     repo_id="openai/clip-vit-base-patch16", local_dir="clip_checkpoints", max_workers=2
+# )

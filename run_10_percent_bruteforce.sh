c51080cdaedf4281d1027b8f91805acf0355c284
hungphongtrn <phonglearntodev@gmail.com>
2025-07-30 10:59:07 +0700

    fix: update dataset name formatting and ensure consistent early stopping parameters in training scripts

--- run_10_percent_bruteforce.sh
+++ run_10_percent_bruteforce.sh
@@ -1,14 +1,14 @@
-uv run trainer.py -cn simple_siglip_only_nitc_mvs \                                                                                                                                                                    
-    dataset.name=CUHK-PEDES \
-    dataset.proportion=0.1 \                                                                                                                                                                                    
-    trainer.max_epochs=200 \                                                                                                                                                                                     
-    loss.SS=true \                                                                                                                                                                                               
-    loss.ss_loss_weight=0.4 \                                                                                                                                                                                    
-    loss.CITC=true \                                                                                                                                                                                             
+uv run trainer.py -cn simple_siglip_only_nitc_mvs \
+    dataset.name='CUHK-PEDES' \
+    dataset.proportion=0.1 \
+    trainer.max_epochs=200 \
+    loss.SS=true \
+    loss.ss_loss_weight=0.4 \
+    loss.CITC=true \
     loss.citc_loss_weight=0.1 \
     trainer.gradient_clip_val=1.0 \
     trainer.gradient_clip_algorithm=norm \
-    +early_stopping.monitor=val_loss \                                                                                                                                                                           
-    +early_stopping.patience=10 \                                                                                                                                                                                
-    +early_stopping.mode=min \                                                                                                                                                                                   
+    +early_stopping.monitor=val_loss \
+    +early_stopping.patience=10 \
+    +early_stopping.mode=min \
     +early_stopping.verbose=true
